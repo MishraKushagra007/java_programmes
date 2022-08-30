046d6ba0ed7fb41967cedb7d7d65fad7fa163762
MishraKushagra007 <92109463+MishraKushagra007@users.noreply.github.com>
2022-08-30 17:18:56 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java .lang.*;
+class odd
+{
+	public static void main(String args[])
+{
+		Scanner input = new Scanner(System.in);
+		System.out.print("enter: ");
+		int a =input.nextInt();
+		if (a%2==0){
+			System.out.println("even");
+		}
+		else{
+			System.out.println("odd");
+}
+}}
+		
+			
